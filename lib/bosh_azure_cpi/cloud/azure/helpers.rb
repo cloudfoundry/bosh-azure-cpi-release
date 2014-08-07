97c2be6078a01ba2e55e60e8d217c0121a794da6
Nicholas Terry <nick.i.terry@gmail.com>
2014-08-06 23:23:42 -0600

    Began implementation of RESTful stemcell request

--- lib/bosh_azure_cpi/cloud/azure/helpers.rb
+++ lib/bosh_azure_cpi/cloud/azure/helpers.rb
@@ -1,4 +1,7 @@
 require 'yaml'
+require 'azure'
+require 'net/https'
+require 'openssl'
 
 module Bosh::AzureCloud
   module Helpers
@@ -27,6 +30,36 @@ module Bosh::AzureCloud
 
     def nil_or_empty?(obj)
       return (obj.nil? || obj.empty?)
+    end
+
+    # TODO: Move to stemcell creator
+    def imageize_vhd(service_name, deployment_name, vm_name)
+      # TODO: Need to set body
+      # See: http://msdn.microsoft.com/en-us/library/azure/dn499768.aspx
+      handle_response post("/#{Azure.subscription_id}/services/hostedservices/#{service_name}/" \
+                           "deployments/#{deployment_name}/roleinstances/#{vm_name}/Operations")
+    end
+
+    private
+
+    def handle_response(response)
+      nil
+    end
+
+    def post(path, body=nil)
+      http = Net::HTTP.new('management.core.windows.net', 443)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.add_field('x-ms-version', '2014-02-01')
+
+
+      # TODO: Need to set body
+      store = OpenSSL::X509::Store.new
+      store.set_default_paths # Optional method that will auto-include the system CAs.
+      store.add_cert(Azure.management_certificate)
+      http.cert_store = store
+
+      response = http.request(Net::HTTP::Post.new(path))
     end
   end
 end
