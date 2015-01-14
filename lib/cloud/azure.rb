90b25e28537a101313959b3ff13f1cebaeb9c288
Nicholas Terry <nicholasterry@familysearch.org>
2015-01-13 23:51:01 -0700

    Moved all classes to proper namespaces where parent cpi expects them to be

---
+++ lib/cloud/azure.rb
@@ -0,0 +1,34 @@
+module Bosh
+  module AzureCloud; end
+end
+
+require 'httpclient'
+require 'pp'
+require 'set'
+require 'tmpdir'
+require 'securerandom'
+require 'yajl'
+
+require 'common/exec'
+require 'common/thread_pool'
+require 'common/thread_formatter'
+
+require 'bosh/registry/client'
+
+require 'cloud'
+require 'cloud/azure/helpers'
+require 'cloud/azure/cloud'
+require 'cloud/azure/version'
+
+require 'cloud/azure/network'
+require 'cloud/azure/dynamic_network'
+require 'cloud/azure/stemcell_manager'
+require 'cloud/azure/virtual_network_manager'
+require 'cloud/azure/vm_manager'
+
+module Bosh
+  module Clouds
+    #TODO: This is a problem as the parent class is also called Azure and we cant change it unless the plugin itself is named something other than just 'Azure'
+    azure = Bosh::AzureCloud::Cloud
+  end
+end
