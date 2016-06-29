7bcab2d630e6eafa7db6fe9c44f52a09d65954d7
Abel Hu <abelch@microsoft.com>
2016-06-29 13:44:05 +0800

    Use 2016-06-01 as RP API version for AzureChinaCloud

--- src/bosh_azure_cpi/lib/cloud/azure/helpers.rb
+++ src/bosh_azure_cpi/lib/cloud/azure/helpers.rb
@@ -26,7 +26,7 @@ module Bosh::AzureCloud
           AZURE_RESOUCE_PROVIDER_COMPUTER         => '2015-06-15',
           AZURE_RESOUCE_PROVIDER_NETWORK          => '2015-06-15',
           AZURE_RESOUCE_PROVIDER_STORAGE          => '2015-06-15',
-          AZURE_RESOUCE_PROVIDER_GROUP            => '2015-06-15',
+          AZURE_RESOUCE_PROVIDER_GROUP            => '2016-06-01',
           AZURE_RESOUCE_PROVIDER_ACTIVEDIRECTORY  => '2015-06-15'
         }
       },
