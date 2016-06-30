e642fcbe8ac52262b55c123bb9ebbde49b33513c
Bin Xia <binxi@microsoft.com>
2016-06-30 08:46:00 +0000

    Check the network resources in the resource group for network

--- ci/tasks/reset-resources.sh
+++ ci/tasks/reset-resources.sh
@@ -12,7 +12,8 @@ function exit_if_error {
 check_param AZURE_TENANT_ID
 check_param AZURE_CLIENT_ID
 check_param AZURE_CLIENT_SECRET
-check_param AZURE_GROUP_NAME
+check_param AZURE_GROUP_NAME_FOR_VMS
+check_param AZURE_GROUP_NAME_FOR_NETWORK
 check_param AZURE_STORAGE_ACCOUNT_NAME
 check_param AZURE_VNET_NAME_FOR_BATS
 check_param AZURE_VNET_NAME_FOR_LIFECYCLE
@@ -24,103 +25,111 @@ azure config mode arm
 
 set +e
 
-echo "Check if the resource group exists"
-echo "azure group list | grep ${AZURE_GROUP_NAME}"
-azure group list | grep ${AZURE_GROUP_NAME}
-
-if [ $? -eq 1 ]; then
-  echo "The task failed because the resource group ${AZURE_GROUP_NAME} does not exist"
-  exit_if_error
-fi
+resource_group_names="${AZURE_GROUP_NAME_FOR_VMS} ${AZURE_GROUP_NAME_FOR_NETWORK}"
+for resource_group_name in ${resource_group_names}
+do
+  echo "Check if the resource group exists"
+  echo "azure group list | grep ${resource_group_name}"
+  azure group list | grep ${resource_group_name}
+  
+  if [ $? -eq 1 ]; then
+    echo "The task failed because the resource group ${resource_group_name} does not exist"
+    exit_if_error
+  fi
+done
 
 set -e
 
 echo "Check if the needed resources exist"
 
-vnets="${AZURE_VNET_NAME_FOR_BATS} ${AZURE_VNET_NAME_FOR_LIFECYCLE}"
-for vnet in $vnets
+resource_group_names="${AZURE_GROUP_NAME_FOR_VMS} ${AZURE_GROUP_NAME_FOR_NETWORK}"
+for resource_group_name in ${resource_group_names}
 do
-  echo "azure network vnet show --resource-group ${AZURE_GROUP_NAME} --name $vnet --json | jq '.name' -r"
-  vnet_actual=$(azure network vnet show --resource-group ${AZURE_GROUP_NAME} --name $vnet --json | jq '.name' -r)
-  if [ "${vnet_actual}" != "$vnet" ]; then
-    echo "The task failed because the virtual network $vnet does not exist"
-    exit_if_error
-  else
-    subnets="${AZURE_BOSH_SUBNET_NAME} ${AZURE_CF_SUBNET_NAME}"
-    for subnet in $subnets
-    do
-      echo "azure network vnet subnet show --resource-group ${AZURE_GROUP_NAME} --vnet-name $vnet --name $subnet --json | jq '.name' -r"
-      subnet_actual=$(azure network vnet subnet show --resource-group ${AZURE_GROUP_NAME} --vnet-name $vnet --name $subnet --json | jq '.name' -r)
-      if [ "${subnet_actual}" != "$subnet" ]; then
-        echo "The task failed because the subnet $subnet in virtual network $vnet does not exist"
-        exit_if_error
-      fi
-    done
-  fi
-done
-
-public_ips="AzureCPICI-bosh AzureCPICI-cf-lifecycle AzureCPICI-cf-bats"
-for public_ip in ${public_ips}
-do
-  echo "azure network public-ip show --resource-group ${AZURE_GROUP_NAME} --name ${public_ip} --json | jq '.name' -r"
-  public_ip_actual=$(azure network public-ip show --resource-group ${AZURE_GROUP_NAME} --name ${public_ip} --json | jq '.name' -r)
-  if [ "${public_ip_actual}" != "${public_ip}" ]; then
-    echo "The task failed because the public IP ${public_ip} does not exist"
-    exit_if_error
-  fi
+  vnets="${AZURE_VNET_NAME_FOR_BATS} ${AZURE_VNET_NAME_FOR_LIFECYCLE}"
+  for vnet in $vnets
+  do
+    echo "azure network vnet show --resource-group ${resource_group_name} --name $vnet --json | jq '.name' -r"
+    vnet_actual=$(azure network vnet show --resource-group ${resource_group_name} --name $vnet --json | jq '.name' -r)
+    if [ "${vnet_actual}" != "$vnet" ]; then
+      echo "The task failed because the virtual network $vnet does not exist in resource group ${resource_group_name}"
+      exit_if_error
+    else
+      subnets="${AZURE_BOSH_SUBNET_NAME} ${AZURE_CF_SUBNET_NAME}"
+      for subnet in $subnets
+      do
+        echo "azure network vnet subnet show --resource-group ${resource_group_name} --vnet-name $vnet --name $subnet --json | jq '.name' -r"
+        subnet_actual=$(azure network vnet subnet show --resource-group ${resource_group_name} --vnet-name $vnet --name $subnet --json | jq '.name' -r)
+        if [ "${subnet_actual}" != "$subnet" ]; then
+          echo "The task failed because the subnet $subnet in virtual network $vnet does not exist in resource group ${resource_group_name}"
+          exit_if_error
+        fi
+      done
+    fi
+  done
+  
+  public_ips="AzureCPICI-bosh AzureCPICI-cf-lifecycle AzureCPICI-cf-bats"
+  for public_ip in ${public_ips}
+  do
+    echo "azure network public-ip show --resource-group ${resource_group_name} --name ${public_ip} --json | jq '.name' -r"
+    public_ip_actual=$(azure network public-ip show --resource-group ${resource_group_name} --name ${public_ip} --json | jq '.name' -r)
+    if [ "${public_ip_actual}" != "${public_ip}" ]; then
+      echo "The task failed because the public IP ${public_ip} does not exist in resource group ${resource_group_name}"
+      exit_if_error
+    fi
+  done
 done
 
 set +e
 
-echo "azure storage account show --resource-group ${AZURE_GROUP_NAME} ${AZURE_STORAGE_ACCOUNT_NAME}"
-azure storage account show --resource-group ${AZURE_GROUP_NAME} ${AZURE_STORAGE_ACCOUNT_NAME}
+echo "azure storage account show --resource-group ${AZURE_GROUP_NAME_FOR_VMS} ${AZURE_STORAGE_ACCOUNT_NAME}"
+azure storage account show --resource-group ${AZURE_GROUP_NAME_FOR_VMS} ${AZURE_STORAGE_ACCOUNT_NAME}
 
 if [ $? -eq 1 ]; then
-  echo "The task failed because the storage account ${AZURE_STORAGE_ACCOUNT_NAME} does not exist"
+  echo "The task failed because the storage account ${AZURE_STORAGE_ACCOUNT_NAME} does not exist in resource group ${AZURE_GROUP_NAME_FOR_VMS}"
   exit_if_error
 fi
 
 set -e
 
-AZURE_ACCOUNT_KEY=$(azure storage account keys list ${AZURE_STORAGE_ACCOUNT_NAME} --resource-group ${AZURE_GROUP_NAME} --json | jq '.storageAccountKeys.key1' -r)
+AZURE_ACCOUNT_KEY=$(azure storage account keys list ${AZURE_STORAGE_ACCOUNT_NAME} --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --json | jq '.storageAccountKeys.key1' -r)
 containers="bosh stemcell"
 for container in $containers
 do
   container_actual=$(azure storage container show --account-name ${AZURE_STORAGE_ACCOUNT_NAME} --account-key ${AZURE_ACCOUNT_KEY} --container $container --json | jq '.name' -r)
   if [ "${container_actual}" != "$container" ]; then
-    echo "The task failed because the container $container does not exist"
+    echo "The task failed because the container $container does not exist in resource group ${AZURE_GROUP_NAME_FOR_VMS}"
     exit_if_error
   fi
 done
 
 echo "Deleting the unneeded resources"
 
-vms=$(azure vm list --resource-group ${AZURE_GROUP_NAME} --json | jq '.[].name' -r)
+vms=$(azure vm list --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --json | jq '.[].name' -r)
 for vm in $vms
 do
-  echo "azure vm delete --resource-group ${AZURE_GROUP_NAME} --name $vm"
-  azure vm delete --resource-group ${AZURE_GROUP_NAME} --name $vm --quiet
+  echo "azure vm delete --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --name $vm"
+  azure vm delete --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --name $vm --quiet
 done
 
-nics=$(azure network nic list --resource-group ${AZURE_GROUP_NAME} --json | jq '.[].name' -r)
+nics=$(azure network nic list --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --json | jq '.[].name' -r)
 for nic in $nics
 do
-  echo "azure network nic delete --resource-group ${AZURE_GROUP_NAME} --name $nic"
-  azure network nic delete --resource-group ${AZURE_GROUP_NAME} --name $nic --quiet
+  echo "azure network nic delete --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --name $nic"
+  azure network nic delete --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --name $nic --quiet
 done
 
-lbs=$(azure network lb list --resource-group ${AZURE_GROUP_NAME} --json | jq '.[].name' -r)
+lbs=$(azure network lb list --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --json | jq '.[].name' -r)
 for lb in $lbs
 do
-  echo "azure network lb delete --resource-group ${AZURE_GROUP_NAME} --name $lb"
-  azure network lb delete --resource-group ${AZURE_GROUP_NAME} --name $lb --quiet
+  echo "azure network lb delete --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --name $lb"
+  azure network lb delete --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --name $lb --quiet
 done
 
-availsets=$(azure availset list --resource-group ${AZURE_GROUP_NAME} --json | jq '.[].name' -r)
+availsets=$(azure availset list --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --json | jq '.[].name' -r)
 for availset in $availsets
 do
-  echo "azure availset delete --resource-group ${AZURE_GROUP_NAME} --name $availset"
-  azure availset delete --resource-group ${AZURE_GROUP_NAME} --name $availset --quiet
+  echo "azure availset delete --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --name $availset"
+  azure availset delete --resource-group ${AZURE_GROUP_NAME_FOR_VMS} --name $availset --quiet
 done
 
 containers="stemcell"
