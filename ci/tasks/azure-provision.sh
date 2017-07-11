c1ae9d000a5d79aaa4da57182164b571a9bd8ba1
Bin Xia <binxi@microsoft.com>
2017-07-11 08:21:34 +0000

    Add lifecycle test for Windows stemcell.
    Other minor fixes and changes:
    
    1. The additional resource groups must be deleted before the default resource groups. Details: The VM OS disk blobs in additional RGs are hosted in the storage account of default RGs. The storage account can't be deleted before the disk blobs are deleted.
    
    2. To save cost, don't create resource groups for CentOS, since the CentOS related tests are not urgent.
    
    3. Use the directory /tmp instead of /mnt when uploading stemcell.

--- ci/tasks/azure-provision.sh
+++ ci/tasks/azure-provision.sh
@@ -12,10 +12,16 @@ set -e
 : ${AZURE_ADDITIONAL_GROUP_NAME_MANAGED_DISKS:?}
 : ${AZURE_DEFAULT_GROUP_NAME_CENTOS:?}
 : ${AZURE_ADDITIONAL_GROUP_NAME_CENTOS:?}
+: ${AZURE_DEFAULT_GROUP_NAME_WINDOWS:?}
+: ${AZURE_ADDITIONAL_GROUP_NAME_WINDOWS:?}
+: ${AZURE_DEFAULT_GROUP_NAME_WINDOWS_MANAGED_DISKS:?}
+: ${AZURE_ADDITIONAL_GROUP_NAME_WINDOWS_MANAGED_DISKS:?}
 : ${AZURE_REGION_NAME:?}
 : ${AZURE_REGION_SHORT_NAME:?}
 : ${AZURE_STORAGE_ACCOUNT_NAME:?}
 : ${AZURE_STORAGE_ACCOUNT_NAME_MANAGED_DISKS:?}
+: ${AZURE_STORAGE_ACCOUNT_NAME_WINDOWS:?}
+: ${AZURE_STORAGE_ACCOUNT_NAME_WINDOWS_MANAGED_DISKS:?}
 : ${AZURE_VNET_NAME_FOR_BATS:?}
 : ${AZURE_VNET_NAME_FOR_LIFECYCLE:?}
 : ${AZURE_BOSH_SUBNET_NAME:?}
@@ -23,37 +29,40 @@ set -e
 : ${AZURE_CF_SUBNET_NAME:?}
 : ${AZURE_CF_SECOND_SUBNET_NAME:?}
 
+function create_storage_account {
+  resource_group_name="$1"
+  storage_account_name="$2"
+  azure storage account create --location ${AZURE_REGION_SHORT_NAME} --sku-name LRS --kind Storage --resource-group ${resource_group_name} ${storage_account_name}
+  storage_account_key=$(azure storage account keys list ${storage_account_name} --resource-group ${resource_group_name} --json | jq '.[0].value' -r)
+  azure storage container create --account-name ${storage_account_name} --account-key ${storage_account_key} --container bosh
+  azure storage container create --account-name ${storage_account_name} --account-key ${storage_account_key} --permission blob --container stemcell
+}
+
 azure login --environment ${AZURE_ENVIRONMENT} --service-principal -u ${AZURE_CLIENT_ID} -p ${AZURE_CLIENT_SECRET} --tenant ${AZURE_TENANT_ID}
 azure config mode arm
 
 set +e
 
-resource_group_names="${AZURE_DEFAULT_GROUP_NAME} ${AZURE_ADDITIONAL_GROUP_NAME} ${AZURE_DEFAULT_GROUP_NAME_MANAGED_DISKS} ${AZURE_ADDITIONAL_GROUP_NAME_MANAGED_DISKS} ${AZURE_DEFAULT_GROUP_NAME_CENTOS} ${AZURE_ADDITIONAL_GROUP_NAME_CENTOS}"
+# The additional resource groups must be deleted before the default resource groups, because the disk blobs of VMs in additional RGs are in the storage account of default RGs.
+# The storage account can't be deleted before the disk blobs are deleted.
+resource_group_names="${AZURE_ADDITIONAL_GROUP_NAME} ${AZURE_DEFAULT_GROUP_NAME} ${AZURE_ADDITIONAL_GROUP_NAME_MANAGED_DISKS} ${AZURE_DEFAULT_GROUP_NAME_MANAGED_DISKS} \
+  ${AZURE_ADDITIONAL_GROUP_NAME_WINDOWS} ${AZURE_DEFAULT_GROUP_NAME_WINDOWS} ${AZURE_ADDITIONAL_GROUP_NAME_WINDOWS_MANAGED_DISKS} ${AZURE_DEFAULT_GROUP_NAME_WINDOWS_MANAGED_DISKS}"
 for resource_group_name in ${resource_group_names}
 do
   # Check if the resource group already exists
-  echo "azure group list | grep ${resource_group_name}"
-  azure group list | grep ${resource_group_name}
+  echo "azure group show --name ${resource_group_name}"
+  azure group show --name ${resource_group_name} > /dev/null 2>&1
   
   if [ $? -eq 0 ]
   then
     echo "azure group delete ${resource_group_name}"
     azure group delete ${resource_group_name} --quiet
-    echo "waiting for delete operation to finish..."
-    # Wait for the completion of deleting the resource group
-    azure group show ${resource_group_name}
-    while [ $? -eq 0 ]
-    do
-      azure group show ${resource_group_name} > /dev/null 2>&1
-      echo "..."
-    done
   fi
 done
 
 set -e
 
 # Create the virtual networks, subnets, and the security groups
-resource_group_names="${AZURE_DEFAULT_GROUP_NAME} ${AZURE_ADDITIONAL_GROUP_NAME} ${AZURE_DEFAULT_GROUP_NAME_MANAGED_DISKS} ${AZURE_ADDITIONAL_GROUP_NAME_MANAGED_DISKS} ${AZURE_DEFAULT_GROUP_NAME_CENTOS} ${AZURE_ADDITIONAL_GROUP_NAME_CENTOS}"
 for resource_group_name in ${resource_group_names}
 do
   echo azure group create ${resource_group_name} ${AZURE_REGION_SHORT_NAME}
@@ -83,15 +92,9 @@ EOF
   azure group deployment create ${resource_group_name} --template-file ./bosh-cpi-src/ci/assets/azure/network.json --parameters-file ./network-parameters.json
 done
 
-# Create the Public IPs
-resource_group_names="${AZURE_DEFAULT_GROUP_NAME} ${AZURE_ADDITIONAL_GROUP_NAME} ${AZURE_DEFAULT_GROUP_NAME_MANAGED_DISKS} ${AZURE_ADDITIONAL_GROUP_NAME_MANAGED_DISKS} ${AZURE_DEFAULT_GROUP_NAME_CENTOS} ${AZURE_ADDITIONAL_GROUP_NAME_CENTOS}"
-for resource_group_name in ${resource_group_names}
-do
-  echo azure network public-ip create --resource-group ${resource_group_name} --name AzureCPICI-cf-lifecycle --location ${AZURE_REGION_SHORT_NAME} --allocation-method Static
-  azure network public-ip create --resource-group ${resource_group_name} --name AzureCPICI-cf-lifecycle --location ${AZURE_REGION_SHORT_NAME} --allocation-method Static
-done
-
-resource_group_names="${AZURE_ADDITIONAL_GROUP_NAME} ${AZURE_ADDITIONAL_GROUP_NAME_MANAGED_DISKS} ${AZURE_ADDITIONAL_GROUP_NAME_CENTOS}"
+# The public IP AzureCPICI-bosh and AzureCPICI-cf-bats are needed only in the additional resource groups
+resource_group_names="${AZURE_ADDITIONAL_GROUP_NAME} ${AZURE_ADDITIONAL_GROUP_NAME_MANAGED_DISKS} \
+  ${AZURE_ADDITIONAL_GROUP_NAME_WINDOWS} ${AZURE_ADDITIONAL_GROUP_NAME_WINDOWS_MANAGED_DISKS}"
 for resource_group_name in ${resource_group_names}
 do
   echo azure network public-ip create --resource-group ${resource_group_name} --name AzureCPICI-bosh --location ${AZURE_REGION_SHORT_NAME} --allocation-method Static
@@ -100,16 +103,8 @@ do
   azure network public-ip create --resource-group ${resource_group_name} --name AzureCPICI-cf-bats --location ${AZURE_REGION_SHORT_NAME} --allocation-method Static
 done
 
-# Setup the storage account
-resource_group_name="${AZURE_DEFAULT_GROUP_NAME}"
-storage_account_name="${AZURE_STORAGE_ACCOUNT_NAME}"
-azure storage account create --location ${AZURE_REGION_SHORT_NAME} --sku-name LRS --kind Storage --resource-group ${resource_group_name} ${storage_account_name}
-storage_account_key=$(azure storage account keys list ${storage_account_name} --resource-group ${resource_group_name} --json | jq '.[0].value' -r)
-azure storage container create --account-name ${storage_account_name} --account-key ${storage_account_key} --container bosh
-azure storage container create --account-name ${storage_account_name} --account-key ${storage_account_key} --permission blob --container stemcell
-
-resource_group_name="${AZURE_DEFAULT_GROUP_NAME_MANAGED_DISKS}"
-storage_account_name="${AZURE_STORAGE_ACCOUNT_NAME_MANAGED_DISKS}"
-azure storage account create --location ${AZURE_REGION_SHORT_NAME} --sku-name LRS --kind Storage --resource-group ${resource_group_name} ${storage_account_name}
-storage_account_key=$(azure storage account keys list ${storage_account_name} --resource-group ${resource_group_name} --json | jq '.[0].value' -r)
-azure storage container create --account-name ${storage_account_name} --account-key ${storage_account_key} --permission blob --container stemcell
+# Setup the storage account only in the default resource groups
+create_storage_account ${AZURE_DEFAULT_GROUP_NAME} ${AZURE_STORAGE_ACCOUNT_NAME}
+create_storage_account ${AZURE_DEFAULT_GROUP_NAME_MANAGED_DISKS} ${AZURE_STORAGE_ACCOUNT_NAME_MANAGED_DISKS}
+create_storage_account ${AZURE_DEFAULT_GROUP_NAME_WINDOWS} ${AZURE_STORAGE_ACCOUNT_NAME_WINDOWS}
+create_storage_account ${AZURE_DEFAULT_GROUP_NAME_WINDOWS_MANAGED_DISKS} ${AZURE_STORAGE_ACCOUNT_NAME_WINDOWS_MANAGED_DISKS}
