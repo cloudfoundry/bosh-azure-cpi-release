791e10f10591fc3933ebbaa69ceca37cb3db2fdc
David Stevenson <dstevenson@pivotal.io>
2019-04-12 10:17:25 -0700

    [CI] New task to just run unit tests
    Signed-off-by: David Stevenson <dstevenson@pivotal.io>

---
+++ ci/tasks/run-unit-tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+source bosh-cpi-src/ci/utils.sh
+source /etc/profile.d/chruby.sh
+chruby ${RUBY_VERSION}
+
+pushd bosh-cpi-src > /dev/null
+  echo "running unit tests"
+  pushd src/bosh_azure_cpi > /dev/null
+    bundle install
+    # AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_ACCESS_KEY are specified fake values as a workaround
+    # to make sure Azure::Storage::Client is mocked successfully in unit tests.
+    # After https://github.com/Azure/azure-storage-ruby/issues/87 is resolved, they can be removed.
+    # echo "bar" | base64 => YmFyCg==
+    AZURE_STORAGE_ACCOUNT="foo" AZURE_STORAGE_ACCESS_KEY="YmFyCg==" bundle exec rspec spec/unit/* --format documentation
+  popd > /dev/null
+popd > /dev/null
