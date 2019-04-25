e1e241e05b2308c16bf28b412c92b35be5ec92fb
Rebecca Putinski <rputinski@pivotal.io>
2019-04-25 13:05:47 -0400

    Update integration tests to fix expectations
    - API version 2 by default, fix missed integration tests
    
    Signed-off-by: David Stevenson <dstevenson@pivotal.io>

--- src/bosh_azure_cpi/spec/integration/migrations/availability_zone_migration_spec.rb
+++ src/bosh_azure_cpi/spec/integration/migrations/availability_zone_migration_spec.rb
@@ -40,6 +40,12 @@ describe Bosh::AzureCloud::Cloud do
     described_class.new(cloud_options_unmanaged, Bosh::AzureCloud::Cloud::CURRENT_API_VERSION)
   end
 
+  subject(:cpi_managed_v1) do
+    cloud_options_unmanaged = @cloud_options.dup
+    cloud_options_unmanaged['azure']['use_managed_disks'] = true
+    described_class.new(cloud_options_unmanaged, 1)
+  end
+
   before { @disk_id_pool = [] }
   after do
     @disk_id_pool.each do |disk_id|
@@ -50,68 +56,88 @@ describe Bosh::AzureCloud::Cloud do
 
   context 'Migrate regional VM to zonal VM', availability_zone: true, migration: true do
     context 'when the regional VM is with unmanaged disks' do
+      before do
+        # Create an unmanaged disk
+        disk_id = cpi_unmanaged.create_disk(2048, {}, nil)
+        expect(disk_id).not_to be_nil
+        @disk_id_pool.push(disk_id)
+      end
+
+      after do
+        # Delete the zonal disk, and the migrated unmanaged regional disk
+        cpi_managed.delete_disk(disk_id)
+        cpi_unmanaged.delete_disk(disk_id)
+        @disk_id_pool.delete(disk_id)
+      end
+
       it 'should exercise the vm lifecycle' do
-        begin
-          # Create an unmanaged disk
-          disk_id = cpi_unmanaged.create_disk(2048, {}, nil)
-          expect(disk_id).not_to be_nil
-          @disk_id_pool.push(disk_id)
-
-          # Create a zonal VM
-          @logger.info("Creating managed zonal VM with stemcell_id='#{@stemcell_id}'")
-          instance_id = cpi_managed.create_vm(SecureRandom.uuid, @stemcell_id, vm_properties, network_spec)
-
-          @logger.info("Checking managed VM existence instance_id='#{instance_id}'")
-          expect(cpi_managed.has_vm?(instance_id)).to be(true)
-
+        vm_lifecycle(cpi: cpi_managed) do |instance_id|
           # Migrate the unmanaged regional disk to a managed zonal disk, and attach the zonal disk to the zonal VM. The disk_id won't be changed.
-          cpi_managed.attach_disk(instance_id, disk_id)
+          @cpi.attach_disk(instance_id, disk_id)
 
           # Detach the zonal disk
           Bosh::Common.retryable(tries: 20, on: Bosh::Clouds::DiskNotAttached, sleep: ->(n, _) { [2**(n - 1), 30].min }) do
-            cpi_managed.detach_disk(instance_id, disk_id)
+            @cpi.detach_disk(instance_id, disk_id)
             true
           end
+        end
+      end
 
-          # Delete the zonal disk, and the migrated unmanaged regional disk
-          cpi_managed.delete_disk(disk_id)
-          cpi_unmanaged.delete_disk(disk_id)
-          @disk_id_pool.delete(disk_id)
-        ensure
-          cpi_managed.delete_vm(instance_id) unless instance_id.nil?
+      context 'when using v1 CPI' do
+        it 'should still exercise the vm lifecycle' do
+          vm_lifecycle(cpi: cpi_managed_v1) do |instance_id|
+            # Migrate the unmanaged regional disk to a managed zonal disk, and attach the zonal disk to the zonal VM. The disk_id won't be changed.
+            @cpi.attach_disk(instance_id, disk_id)
+
+            # Detach the zonal disk
+            Bosh::Common.retryable(tries: 20, on: Bosh::Clouds::DiskNotAttached, sleep: ->(n, _) { [2**(n - 1), 30].min }) do
+              @cpi.detach_disk(instance_id, disk_id)
+              true
+            end
+          end
         end
       end
     end
 
     context 'when the regional VM is with managed disks' do
+      before do
+        # Create an regional disk
+        disk_id = cpi_managed.create_disk(2048, {}, nil)
+        expect(disk_id).not_to be_nil
+        @disk_id_pool.push(disk_id)
+      end
+
+      after do
+        # Delete the zonal disk
+        cpi_managed.delete_disk(disk_id)
+        @disk_id_pool.delete(disk_id)
+      end
+
       it 'should exercise the vm lifecycle' do
-        begin
-          # Create an regional disk
-          disk_id = cpi_managed.create_disk(2048, {}, nil)
-          expect(disk_id).not_to be_nil
-          @disk_id_pool.push(disk_id)
-
-          # Create a zonal VM
-          @logger.info("Creating managed zonal VM with stemcell_id='#{@stemcell_id}'")
-          instance_id = cpi_managed.create_vm(SecureRandom.uuid, @stemcell_id, vm_properties, network_spec)
-
-          @logger.info("Checking zonal VM existence instance_id='#{instance_id}'")
-          expect(cpi_managed.has_vm?(instance_id)).to be(true)
-
-          # Migrate the regional disk to a zonal disk, and attach the zonal disk to the zonal VM. The disk_id won't be changed.
-          cpi_managed.attach_disk(instance_id, disk_id)
+        vm_lifecycle(cpi: cpi_managed) do |instance_id|
+          # Migrate the unmanaged regional disk to a managed zonal disk, and attach the zonal disk to the zonal VM. The disk_id won't be changed.
+          @cpi.attach_disk(instance_id, disk_id)
 
           # Detach the zonal disk
           Bosh::Common.retryable(tries: 20, on: Bosh::Clouds::DiskNotAttached, sleep: ->(n, _) { [2**(n - 1), 30].min }) do
-            cpi_managed.detach_disk(instance_id, disk_id)
+            @cpi.detach_disk(instance_id, disk_id)
             true
           end
+        end
+      end
 
-          # Delete the zonal disk
-          cpi_managed.delete_disk(disk_id)
-          @disk_id_pool.delete(disk_id)
-        ensure
-          cpi_managed.delete_vm(instance_id) unless instance_id.nil?
+      context 'when using v1 CPI' do
+        it 'should still exercise the vm lifecycle' do
+          vm_lifecycle(cpi: cpi_managed_v1) do |instance_id|
+            # Migrate the unmanaged regional disk to a managed zonal disk, and attach the zonal disk to the zonal VM. The disk_id won't be changed.
+            @cpi.attach_disk(instance_id, disk_id)
+
+            # Detach the zonal disk
+            Bosh::Common.retryable(tries: 20, on: Bosh::Clouds::DiskNotAttached, sleep: ->(n, _) { [2**(n - 1), 30].min }) do
+              @cpi.detach_disk(instance_id, disk_id)
+              true
+            end
+          end
         end
       end
     end
