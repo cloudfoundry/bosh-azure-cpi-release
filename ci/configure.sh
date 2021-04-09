934fa0fb262dfdedce930e117d023b0e27cf030d
Joseph Palermo <jpalermo@pivotal.io>
2021-04-09 14:46:18 -0700

    Build docker image in ci

--- ci/configure.sh
+++ ci/configure.sh
@@ -6,7 +6,9 @@ if [[ $(lpass status -q; echo $?) != 0 ]]; then
   echo "Login with lpass first"
   exit 1
 fi
+FLY="${FLY_CLI:-fly}"
 
-fly -t main-cpi set-pipeline -p "bosh-azure-cpi" \
+${FLY} -t "${CONCOURSE_TARGET:-main}" set-pipeline -p "bosh-azure-cpi" \
     -c ci/pipeline.yml \
-    --load-vars-from <(lpass show -G --sync=now "azure-cpi-bats-concourse-secrets" --notes)
+    --load-vars-from <(lpass show -G --sync=now "azure-cpi-bats-concourse-secrets" --notes) \
+    --load-vars-from <(lpass show -G --sync=now "bosh:docker-images concourse secrets" --notes)
