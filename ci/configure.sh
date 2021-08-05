8cce378d0cc368ec5119b858fb63797b53888276
Joseph Palermo <jpalermo@pivotal.io>
2021-08-05 00:52:52 -0700

    Move pipeline to bosh-ecosystem concourse

--- ci/configure.sh
+++ ci/configure.sh
@@ -8,7 +8,7 @@ if [[ $(lpass status -q; echo $?) != 0 ]]; then
 fi
 FLY="${FLY_CLI:-fly}"
 
-${FLY} -t "${CONCOURSE_TARGET:-main}" set-pipeline -p "bosh-azure-cpi" \
+${FLY} -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline -p "bosh-azure-cpi" \
     -c ci/pipeline.yml \
     --load-vars-from <(lpass show -G --sync=now "azure-cpi-bats-concourse-secrets" --notes) \
     --load-vars-from <(lpass show -G --sync=now "bosh:docker-images concourse secrets" --notes)
