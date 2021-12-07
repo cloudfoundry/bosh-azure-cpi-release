5467d595d57fe907eff2181ed5d4757364037821
Justin Webster <justin.webster@gmail.com>
2021-12-07 15:28:53 -0800

    Added a missing `describe` block in `bosh_agent_util_spec.rb`.
    Note: This new describe block makes the spec file more consistent with standard RSpec and rubocop guidelines.

--- src/bosh_azure_cpi/spec/unit/utils/bosh_agent_util_spec.rb
+++ src/bosh_azure_cpi/spec/unit/utils/bosh_agent_util_spec.rb
@@ -33,77 +33,78 @@ describe Bosh::AzureCloud::BoshAgentUtil do
     allow(config).to receive(:agent).and_return({'mbus' => 'http://u:p@somewhere'})
   end
 
-  context 'when using registry' do
-    let(:uses_registry) { true }
-    let(:expected_user_data) do
-      {
-          registry: { endpoint: registry_endpoint },
+  describe '#user_data_obj' do
+    context 'when using registry' do
+      let(:uses_registry) { true }
+      let(:expected_user_data) do
+        {
+            registry: { endpoint: registry_endpoint },
+            server: { name: instance_id },
+            dns: { nameserver: dns }
+        }
+      end
+
+      it 'user data is for registry usage' do
+        user_data = agent_util.user_data_obj(
+          registry_endpoint,
+          instance_id,
+          dns,
+          agent_id,
+          network_spec,
+          environment,
+          vm_params,
+          config,
+        )
+
+        expect(user_data).to eq(expected_user_data)
+      end
+    end
+
+    context 'when not using registry' do
+      let(:uses_registry) { false }
+      let(:expected_user_data) do
+        {
           server: { name: instance_id },
-          dns: { nameserver: dns }
-      }
+          dns: { nameserver: dns },
+          'vm' => {'name' => vm_params[:name]},
+          'agent_id' => agent_id,
+          'networks' => {
+            'network_a' => {
+              'type' => 'dynamic',
+              'cloud_properties' => {
+                'virtual_network_name' => 'vnet_name',
+                'subnet_name' => 'subnet_name'
+              },
+              'use_dhcp' => true
+            }
+          },
+          'disks' => {
+            'system' => '/dev/sda',
+            'persistent' => {},
+            'ephemeral' => {
+              'lun' => "0",
+              'host_device_id' => '{f8b3781b-1e82-4818-a1c3-63d806ec15bb}',
+            }
+          },
+          'env' => environment,
+          'mbus' => 'http://u:p@somewhere',
+        }
+      end
+
+      it 'combines the reduced vm metadata with agent settings and removes registry' do
+        user_data = agent_util.user_data_obj(
+          registry_endpoint,
+          instance_id,
+          dns,
+          agent_id,
+          network_spec,
+          environment,
+          vm_params,
+          config,
+        )
+
+        expect(user_data).to eq(expected_user_data)
+      end
     end
-
-    it 'user data is for registry usage' do
-      user_data = agent_util.user_data_obj(
-        registry_endpoint,
-        instance_id,
-        dns,
-        agent_id,
-        network_spec,
-        environment,
-        vm_params,
-        config,
-      )
-
-      expect(user_data).to eq(expected_user_data)
-    end
-  end
-
-  context 'when not using registry' do
-    let(:uses_registry) { false }
-    let(:expected_user_data) do
-      {
-        server: { name: instance_id },
-        dns: { nameserver: dns },
-        'vm' => {'name' => vm_params[:name]},
-        'agent_id' => agent_id,
-        'networks' => {
-          'network_a' => {
-            'type' => 'dynamic', 
-            'cloud_properties' => {
-              'virtual_network_name' => 'vnet_name', 
-              'subnet_name' => 'subnet_name'
-            },
-            'use_dhcp' => true
-          }
-        },
-        'disks' => {
-          'system' => '/dev/sda',
-          'persistent' => {},
-          'ephemeral' => {
-            'lun' => "0",
-            'host_device_id' => '{f8b3781b-1e82-4818-a1c3-63d806ec15bb}',
-          }
-        },
-        'env' => environment,
-        'mbus' => 'http://u:p@somewhere',
-      }
-    end
-
-    it 'combines the reduced vm metadata with agent settings and removes registry' do
-      user_data = agent_util.user_data_obj(
-        registry_endpoint,
-        instance_id,
-        dns,
-        agent_id,
-        network_spec,
-        environment,
-        vm_params,
-        config,
-      )
-
-      expect(user_data).to eq(expected_user_data)
-    end
-
   end
 end
