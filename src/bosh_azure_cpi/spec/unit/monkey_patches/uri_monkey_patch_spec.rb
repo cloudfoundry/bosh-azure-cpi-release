a478caaae98fab8e53267b9d92b37d148e48393a
Aram Price <pricear@vmware.com>
2022-03-16 11:34:09 -0500

    Monkey Patch: URI.escape
    Fixes the problems related to the bump to ruby 3.1. From ruby version 3
    the URI.escape function was removed. This is a monkey patch to add that
    function again and restore the broken functionality.
    
    [#181531703] [azure cpi] Fix the Docker image that builds the azure CPI
    
    Signed-off-by: Daniel Felipe Ochoa <danielfelipo@vmware.com>

---
+++ src/bosh_azure_cpi/spec/unit/monkey_patches/uri_monkey_patch_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'monkey_patches/uri_monkey_patch'
+describe Bosh::AzureCloud::URIMonkeyPatch do
+  describe '.apply_patch' do
+    context 'when URI does not have an escape method' do
+      it 'should add the escape function' do
+        expect { URI.method(:escape) }.to raise_error(NameError)
+        expect { Bosh::AzureCloud::URIMonkeyPatch.apply_patch }.not_to raise_error
+        expect { URI.method(:escape) }.not_to raise_error
+      end
+
+      context 'and azure-storage-table library is not 2.0.4' do
+        let(:mismatch_version) { '2.0.5' }
+
+        before do
+          allow(::Azure::Storage::Table::Version).to receive(:to_s).and_return(mismatch_version)
+        end
+
+        it 'should raise an exception' do
+          expect { Bosh::AzureCloud::URIMonkeyPatch.apply_patch }.to raise_error(Bosh::AzureCloud::URIMonkeyPatch::AZURE_VERSION_MISMATCH_WARNING)
+        end
+      end
+    end
+
+    context 'when URI has an escape method' do
+      before do
+        allow(::URI).to receive(:method_defined?).with(:escape).and_return(true)
+      end
+
+      it 'should not modify the current escape method' do
+        allow(URI).to receive(:class_eval)
+
+        expect { Bosh::AzureCloud::URIMonkeyPatch.apply_patch }.not_to raise_error
+        expect(URI).not_to receive(:class_eval)
+      end
+    end
+  end
+end
