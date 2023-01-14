809399c1eb246db05ceac4fe991c06b194819fd1
Joseph Palermo <jpalermo@pivotal.io>
2023-01-13 16:38:01 -0800

    Update ci pipeline with new credhub entries

--- ci/configure.sh
+++ ci/configure.sh
@@ -10,6 +10,4 @@ FLY="${FLY_CLI:-fly}"
 
 ${FLY} -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline -p "bosh-azure-cpi" \
     -c ci/pipeline.yml \
-    -v ruby_version=$(cat src/bosh_azure_cpi/.ruby-version) \
-    --load-vars-from <(lpass show -G --sync=now "azure-cpi-bats-concourse-secrets" --notes) \
-    --load-vars-from <(lpass show -G --sync=now "bosh:docker-images concourse secrets" --notes)
+    -v ruby_version=$(cat src/bosh_azure_cpi/.ruby-version)
