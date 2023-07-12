2869bc9074dc9ecb298c20fd5f60812c246d6823
Joseph Palermo <jpalermo@pivotal.io>
2023-07-12 20:44:01 +0800

    Update repo url to cloudfoundry org

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,10 +2,6 @@
 
 set -eu
 
-if [[ $(lpass status -q; echo $?) != 0 ]]; then
-  echo "Login with lpass first"
-  exit 1
-fi
 FLY="${FLY_CLI:-fly}"
 
 ${FLY} -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline -p "bosh-azure-cpi" \
