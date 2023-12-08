0fadf2f7e8e49a1a36d494ab7a460a99ad2f6cc6
Joseph Palermo <jpalermo@pivotal.io>
2023-12-07 16:13:05 -0800

    Update bats config generation to account for changes in bats

--- ci/tasks/run-az-bats.sh
+++ ci/tasks/run-az-bats.sh
@@ -5,7 +5,6 @@ set -e
 : ${INFRASTRUCTURE:?}
 : ${STEMCELL_NAME:?}
 : ${BAT_INFRASTRUCTURE:?}
-: ${BAT_NETWORKING:?}
 : ${BAT_RSPEC_FLAGS:?}
 : ${AZURE_BATS_ZONE:?}
 
@@ -19,19 +18,14 @@ mkdir -p bats-config
 bosh int bosh-cpi-src/ci/tasks/az-bats-spec.yml \
   -v "stemcell_name=${STEMCELL_NAME}" \
   -v "availability_zone=${AZURE_BATS_ZONE}"  \
+  -v "ssh_private_key=$( creds_path /jumpbox_ssh/private_key )" \
+  -v "ssh_public_key=$( creds_path /jumpbox_ssh/public_key )" \
   -l environment/metadata > bats-config/bats-config.yml
 
 source director-state/director.env
-export BAT_PRIVATE_KEY="$( creds_path /jumpbox_ssh/private_key )"
-export BAT_DNS_HOST="${BOSH_ENVIRONMENT}"
 export BAT_STEMCELL=$(realpath stemcell/*.tgz)
 export BAT_DEPLOYMENT_SPEC=$(realpath bats-config/bats-config.yml)
 export BAT_BOSH_CLI=$(which bosh)
-
-ssh_key_path=/tmp/bat_private_key
-echo "$BAT_PRIVATE_KEY" > $ssh_key_path
-chmod 600 $ssh_key_path
-export BOSH_GW_PRIVATE_KEY=$ssh_key_path
 
 pushd bats
   bundle install
