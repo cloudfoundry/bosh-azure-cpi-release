a4c21414dfd52cd1a6ca88ffeca2e9b93c918410
Kenneth Lakin <kenneth.lakin@broadcom.com>
2024-07-29 16:48:51 -0700

    Dynamically sets RUBY_VERSION in two tasks
    This commit moves the responsibility for setting 'RUBY_VERSION' in the
    'bats-managed-disks' and 'bats-unmanaged-disks' Tasks from the CI
    configuration script to Concourse, itself.
    
    Why? We now have CI-run automation that updates
    'src/bosh_azure_cpi/.ruby-version'. Getting the credentials to this
    automation to have it automatically reconfigure the relevant pipelines
    is more of a hassle then using this fairly-new Concourse feature.
    
    NOTE: This commit uses the `load_var` step, which requires Concourse
    v7.9.0 or later:
    <https://github.com/concourse/concourse/releases/tag/v7.9.0>.
    
    Hopefully permanently resolves the following failure in CI:
    ```
    task: deploy-director
    chruby: unknown Ruby: 3.2.4
    ```

--- ci/configure.sh
+++ ci/configure.sh
@@ -5,5 +5,4 @@ set -eu
 FLY="${FLY_CLI:-fly}"
 
 ${FLY} -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline -p "bosh-azure-cpi" \
-    -c ci/pipeline.yml \
-    -v ruby_version=$(cat src/bosh_azure_cpi/.ruby-version)
+    -c ci/pipeline.yml
