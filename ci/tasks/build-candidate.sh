52926c15b3a36391e97739941c2ea9c40dbc43c9
Ming Xiao <ming.xiao@broadcom.com>
2024-11-05 13:44:49 -0800

    Use migrated gcloud bucket instead of s3
    No longer build and update a dev release, create the dev release when needed within the job
    
    Remove promote-candidate.yml and use shared task in bosh-shared-ci
    
    Signed-off-by: Ming Xiao <ming.xiao@broadcom.com>

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -2,23 +2,9 @@
 
 set -e
 
-source bosh-cpi-src/ci/utils.sh
-
-semver=`cat version-semver/number`
-
 pushd bosh-cpi-src > /dev/null
-  echo "running unit tests"
-  pushd src/bosh_azure_cpi > /dev/null
-    bundle install
-    # AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_ACCESS_KEY are specified fake values as a workaround
-    # to make sure Azure::Storage::Common::Client is mocked successfully in unit tests.
-    # After https://github.com/Azure/azure-storage-ruby/issues/87 is resolved, they can be removed.
-    # echo "bar" | base64 => YmFyCg==
-    AZURE_STORAGE_ACCOUNT="foo" AZURE_STORAGE_ACCESS_KEY="YmFyCg==" bundle exec rspec spec/unit/* --format documentation
-  popd > /dev/null
-
   cpi_release_name="bosh-azure-cpi"
 
   echo "building CPI release..."
-  bosh create-release --name $cpi_release_name --version $semver --tarball ../candidate/$cpi_release_name-$semver.tgz
+  bosh create-release --name $cpi_release_name --tarball ../cpi-release/$cpi_release_name-dev.tgz
 popd > /dev/null
