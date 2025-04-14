57161cb753b8b052a6586b9153898455019a17e1
Sebastian Heid <8442432+s4heid@users.noreply.github.com>
2025-04-14 12:27:29 +0200

    Allow capcity reservation groups in `create_vm`
    When `capacity_reservation_groups` are specified in the cloud_properties of a vm
    type / extension, propagate the group to the create_vm api call.

--- src/bosh_azure_cpi/lib/cloud/azure/restapi/azure_client.rb
+++ src/bosh_azure_cpi/lib/cloud/azure/restapi/azure_client.rb
@@ -336,6 +336,19 @@ module Bosh::AzureCloud
         }
       }
 
+      capacity_reservation_group_id = vm_params[:capacity_reservation_group]
+      unless capacity_reservation_group_id.nil?
+        unless vm_params[:capacity_reservation_group].start_with?('/subscriptions')
+          capacity_reservation_group_id = rest_api_url(REST_API_PROVIDER_COMPUTE, 'capacityReservationGroups', resource_group_name: resource_group_name, name: vm_params[:capacity_reservation_group])
+        end
+
+        vm['properties']['capacityReservation'] = {
+          'capacityReservationGroup' => {
+            'id' => capacity_reservation_group_id
+          }
+        }
+      end
+
       unless vm_params[:identity].nil?
         identity_type = vm_params[:identity][:type]
         if identity_type == MANAGED_IDENTITY_TYPE_USER_ASSIGNED
